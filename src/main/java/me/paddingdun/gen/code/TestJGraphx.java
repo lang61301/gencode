08d2649bf1f241d343a53e3ebad68dadf44c4f1c
paddingdun <gaokun_xf@126.com>
2015-12-10 11:04:47 +0800

    修正可以获取mysql表的注释

--- src/main/java/me/paddingdun/gen/code/TestJGraphx.java
+++ src/main/java/me/paddingdun/gen/code/TestJGraphx.java
@@ -7,6 +7,9 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.util.Properties;
+
+import com.mysql.jdbc.DatabaseMetaDataUsingInfoSchema;
 
 /**
  * @author paddingdun
@@ -19,10 +22,13 @@ public class TestJGraphx {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = null;
 		ResultSet rs = null;
-		
-		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ssh2?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull", "root", "root");
-		
+		Properties props = new Properties();
+		props.setProperty("user", "root");
+		props.setProperty("password", "root");
+		props.setProperty("useInformationSchema", "true");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ssh2?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull", props);
 		DatabaseMetaData dmd =  con.getMetaData();
+		System.out.println((dmd instanceof DatabaseMetaDataUsingInfoSchema));
 		rs = dmd.getSchemas();
 		while(rs.next()){
 			System.out.println(rs.getString("TABLE_SCHEM"));
@@ -35,7 +41,7 @@ public class TestJGraphx {
 		}
 		rs.close();
 		System.out.println("---------------");
-		rs = dmd.getTables("ssh2", null, null, null);
+		rs = dmd.getTables("pms", null, null, null);
 		while(rs.next()){
 //			int  i = rs.getMetaData().getColumnCount();
 //			for (int j = 0; j < i; j++) {
@@ -43,6 +49,7 @@ public class TestJGraphx {
 //			}
 			System.out.println(rs.getString("TABLE_NAME"));
 			System.out.println(rs.getString("TABLE_TYPE"));
+			System.out.println(rs.getString("REMARKS"));
 		}
 		rs.close();
 		System.out.println("---------------");
